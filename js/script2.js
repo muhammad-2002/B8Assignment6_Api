a09ab7a4b7b4e82376a6445a1b45e83cac392c48
muhammad-2002 <masumbilla10104@gmail.com>
2024-03-02 18:00:21 +0600

    final commit

--- js/script2.js
+++ js/script2.js
@@ -3,6 +3,7 @@ const getTheCategories =async ()=>{
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`)
     const data = await res.json();
     console.log(data.data)
+    
     displayCategories(data.data)
     
 
@@ -23,14 +24,48 @@ const displayCategories =(categories)=>{
 //load corresponding category data
 
 const getData= async(category_id ='1000')=>{
+    spenner(true)
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`)
     const data = await res.json();
-    displayDataToCard(data.data)
-
-    console.log(data.data)
+    const cardArr = data.data
+    
+    displayDataToCard(cardArr);
+    
+    
+    document.getElementById('sortBybtn').addEventListener('click',()=>{
+        spenner(true)
+        displaySortedData(cardArr);
+        
+    })
+    
+    
+   
 
 }
+//display to sort view data when click sort by view
+const displaySortedData = (cardArr)=>{
+
+    cardArr.forEach((element)=>{
+        const views = element.others?.views
+        element.viewsNumber = convertNumbers(views);
+    })
+    
+    cardArr.sort((a,b)=>b.viewsNumber - a.viewsNumber);
+    
+    displayDataToCard(cardArr)
+    console.log(cardArr)
+    
+}
+const convertNumbers=(views)=>{
+    const multiplyValue = views.endsWith('K')? 1000: 1;
+    return parseFloat(views)*multiplyValue;
+
+}
+
+
 const displayDataToCard=(cards)=>{
+    
+    
     
     const cardContainer = document.getElementById('cardContainer')
     cardContainer.innerHTML='';
@@ -42,14 +77,19 @@ const displayDataToCard=(cards)=>{
         noDataContainer.classList.add('hidden')
     }
     cards.forEach(element=>{
+        const sec_time = element.others?.posted_date ;
+        const timeZone = ConvertHours(sec_time);
+        
+        
         const newDiv = document.createElement('div')
         newDiv.innerHTML =`<div class="card bg-base-100 min-h-[380px] shadow-xl">
-        <figure>
+        <figure class='relative'>
           <img class='h-[200px] w-full'
             src="${element.thumbnail}"
             alt="Shoes"
           />
         </figure>
+        <div class='bg-black p-2 text-white font-bold absolute top-3 right-3'>${timeZone}</div>
         <div class="card-body">
         <div class ='flex items-center gap-6'><img class ='w-[40px] h-[40px] rounded-full'
         src="${element.authors[0].profile_picture}"
@@ -60,7 +100,11 @@ const displayDataToCard=(cards)=>{
         </div>
       </div>`
       cardContainer.appendChild(newDiv)
+      spenner(false)
     })
+    
+   
+    //convert hours  and minute
 
     // display no data found 
     const div = document.createElement('div');
@@ -73,8 +117,37 @@ const displayDataToCard=(cards)=>{
     </h1>
     `;
     noDataContainer.appendChild(div);
+    spenner(false)
+    
+    
+    
+}
+
+const ConvertHours =(sec_time)=>{
+
+    const hr = Math.floor(sec_time/3600);
+    const remainSec = (sec_time%3600)
+    const minute = Math.floor(remainSec/60);
+    if(hr === 0 && minute ===0){
+        return ''
+    }
+    else{
+        return `${hr} hours ${minute} minute`
+        
+    }
+    
+}
+
+const spenner =(isSpring)=>{
+    if(isSpring){
+        document.getElementById('spenner').classList.remove('hidden')
+    }else{
+        document.getElementById('spenner').classList.add('hidden')
+    }
 
 }
+
+
 getData()
 
 //call get categories
